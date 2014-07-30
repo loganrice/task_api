a8b5bd6e5049581a2ed9368c6195c6c9ad9cdbdc
loganrice <loganrice72@gmail.com>
2014-07-30 08:29:56 -0700

    set up models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeoChecklist::Application.config.secret_key_base = '9da6a4ec6ebe7ba3877c6bd99cdce6659dcc98125e1a2466196fb69a96523dbf060198be55e44abd78c75dd5dda1f007eed0ed3fe71901b9a46fbff36dfa5d4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SeoChecklist::Application.config.session_store :cookie_store, key: '_seo_checklist_session'
